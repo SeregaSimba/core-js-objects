a6cc573af3d485e8e037bec23e0fa188e87fe431
Sergei <143517331+SeregaSimba@users.noreply.github.com>
2025-03-08 17:52:36 +0300

    feat: function 5

--- src/objects-tasks.js
+++ src/objects-tasks.js
@@ -128,8 +128,9 @@ function isEmptyObject(obj) {
  *    immutableObj.newProp = 'new';
  *    console.log(immutableObj) => {a: 1, b: 2}
  */
-function makeImmutable(/* obj */) {
-  throw new Error('Not implemented');
+function makeImmutable(obj) {
+  const copyObj = Object.freeze(obj);
+  return copyObj;
 }
 
 /**

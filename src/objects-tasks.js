3b76ff152065a3e14fd20824548216d06bb91a26
Sergei <143517331+SeregaSimba@users.noreply.github.com>
2025-03-08 17:48:38 +0300

    feat: function 4

--- src/objects-tasks.js
+++ src/objects-tasks.js
@@ -103,8 +103,13 @@ function compareObjects(obj1, obj2) {
  *    isEmptyObject({}) => true
  *    isEmptyObject({a: 1}) => false
  */
-function isEmptyObject(/* obj */) {
-  throw new Error('Not implemented');
+function isEmptyObject(obj) {
+  const copyObj = Object.keys(obj);
+
+  if (copyObj.length === 0) {
+    return true;
+  }
+  return false;
 }
 
 /**

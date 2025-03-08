720cf486bf7a879909e134dfe6efbd91136bdb9a
Sergei <143517331+SeregaSimba@users.noreply.github.com>
2025-03-08 16:59:37 +0300

    feat: function 2

--- src/objects-tasks.js
+++ src/objects-tasks.js
@@ -61,8 +61,13 @@ function mergeObjects(objects) {
  *    removeProperties({name: 'John', age: 30, city: 'New York'}, ['age']) => {name: 'John', city: 'New York'}
  *
  */
-function removeProperties(/* obj, keys */) {
-  throw new Error('Not implemented');
+function removeProperties(obj, keys) {
+  const ob = obj;
+
+  keys.forEach((key) => {
+    delete ob[key];
+  });
+  return ob;
 }
 
 /**

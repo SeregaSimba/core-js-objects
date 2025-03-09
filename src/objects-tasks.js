9d9a7815a82f1d181ef28e6384a940f1d1fb1b2d
Sergei <143517331+SeregaSimba@users.noreply.github.com>
2025-03-09 17:16:51 +0300

    feat: function 7

--- src/objects-tasks.js
+++ src/objects-tasks.js
@@ -172,8 +172,35 @@ function makeWord(lettersObject) {
  *    sellTickets([25, 25, 50]) => true
  *    sellTickets([25, 100]) => false (The seller does not have enough money to give change.)
  */
-function sellTickets(/* queue */) {
-  throw new Error('Not implemented');
+function sellTickets(queue) {
+  let result25 = 0;
+  let result50 = 0;
+
+  // const objTicks = Object.entries(queue);
+  for (let i = 0; i < queue.length; i += 1) {
+    const elem = queue[i];
+
+    if (elem === 25) {
+      result25 += 1;
+    } else if (elem === 50) {
+      if (result25 > 0) {
+        result25 -= 1;
+        result50 += 1;
+      } else {
+        return false;
+      }
+    } else if (elem === 100) {
+      if (result50 > 0 && result25 > 0) {
+        result50 -= 1;
+        result25 -= 1;
+      } else if (result25 >= 3) {
+        result25 -= 3;
+      } else {
+        return false;
+      }
+    }
+  }
+  return true;
 }
 
 /**
